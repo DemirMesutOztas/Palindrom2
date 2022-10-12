cc98a1ba0261fa00125149efd528339c4a812df8
Gogle Öztaş <demiroztas06@hotmail.com>
2022-10-12 14:00:13 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static boolean palindrom(int a)
+    {
+        int orj=a;
+        int b = a;
+
+        int ters=0;
+        while(a>0)
+        {
+            b=a%10;
+            ters=ters*10+b;
+            a = a/10;
+
+
+        }
+
+        if(ters==orj)
+            return true;
+        else
+            return false;
     }
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sayı giriniz:");
+        int a =  input.nextInt();
+
+        boolean durum =  palindrom(a);
+
+        if (durum)
+            System.out.println("Palindrom");
+        else
+            System.out.println("Palindrom değil");
+    }
+
 }
